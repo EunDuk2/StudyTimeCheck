258b9408e24c19c015017c9f5774e01736186534
eunsung <dmsqls6789@naver.com>
2023-07-27 16:41:44 +0900

    최초 커밋

---
+++ StudyTimeCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyTimeCheck
+//
+//  Created by EUNSUNG on 2023/07/27.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
