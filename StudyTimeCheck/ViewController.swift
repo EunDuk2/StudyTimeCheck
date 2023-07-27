e785340bffe68f4d0f44b6e581f8b1c6f567e9bf
eunsung <dmsqls6789@naver.com>
2023-07-27 16:47:16 +0900

    최초 실행

--- StudyTimeCheck/ViewController.swift
+++ StudyTimeCheck/ViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 import RealmSwift
 
 class ViewController: UIViewController {
+    
+    let realm = try! Realm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Realm 파일 위치
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
     }
-
+    
 
 }
 
+// 학생 클래스
+class Student: Object {
+    @objc dynamic var name: String = "" // 이름
+    @objc dynamic var number: String = "" // 학번
+    
+    convenience init(name: String, number: String) {
+        self.init()
+        self.name = name
+        self.number = number
+    }
+}
